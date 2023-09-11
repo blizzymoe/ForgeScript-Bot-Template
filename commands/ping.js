bc4601eba4c8df508abb06b90386b402661acf50
Yumi Nanase <107496047+yuminanase@users.noreply.github.com>
2023-09-11 19:52:36 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,13 @@
 module.exports = {
     name: "ping",
     type: "messageCreate",
-    code: `🏓 | Pong! I responded in $pingms.`
+    code: `$let[msg;$sendMessage[$channelID;$description[Checking client latency...]
+$color[FFFFFF];true]]
+$let[1st;$ping]
+$wait[1000]
+$function[$editMessage[$channelID;$get[msg];$title[🏓 | Pong!]
+$description[**Client Latency:** \`$get[1st]ms\`
+**Roundtrip:** \`$math[$executionTime-1000]ms\`
+**Database:** \`$cropText[$randomNumber[0;1;true];0;3]ms\`]
+$color[FFFFFF]]`
 }
