c6060ca42ffffb3b09a6e4585a25e9d11a959a7f
Yumi Nanase <107496047+yuminanase@users.noreply.github.com>
2023-09-11 19:59:39 +0800

    Create updateCommands.js

---
+++ commands/updateCommands.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "updatecmnd",
+    aliases: ["update", "cmnd", "up"],
+    type: "messageCreate",
+    code: `$if[$authorID==$botOwnerID;$updateCommands $function[$addMessageReactions[$channelID;$messageID;✅]]]`
+}
