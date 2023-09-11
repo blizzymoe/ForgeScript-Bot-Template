11c56578465ffa5a52da20f14334428106db1cdc
Yumi Nanase <107496047+yuminanase@users.noreply.github.com>
2023-09-11 19:51:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,38 +7,23 @@ const client = new ForgeClient({
     ],
     
   intents: [
-    "GuildMembers",
+    "DirectMessages",
     "GuildMessages",
     "Guilds",
-    "GuildPresences",
     "MessageContent" // This intent is privileged, must be whitelisted in dev portal, in your application.
   ],
 
   events: [
-    "messageUpdate",
-    "messageDelete",
-    "guildMemberAdd",
-    "guildMemberRemove",
-    "userUpdate",
     "messageCreate",
     "ready"
   ], // Events our bot will act on
 
   prefixes: [
     "!",
-    "?",
-    "."
+    "?"
   ] // The prefixes to use for our bot!
+  useInviteSystem: true
   })
 
 client.commands.load("./commands")
-client.commands.add({
-    name: "", // Not defining this creates a command that will be executed for every event fired of given type
-    code: `$setStatus[online;Streaming;with $userCount users in $guildCount guilds;;https://twitch.tv/forgify]`,
-    type: "ready" // The event to act on
-})
-client.commands.add({
-    name: "forge", // Not defining this creates a command that will be executed for every event fired of given type
-    code: `$if[$authorID==$botOwnerID;$eval[$message;true];$function[$addMessageReactions[$channelID;$messageID;❌]]]`,
-    type: "messageCreate" // The event to act on
-})
+client.login("bot_token") // You can get your bot token on https://discord.com/developers/applications
