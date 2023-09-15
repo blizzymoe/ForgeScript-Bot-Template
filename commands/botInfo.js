1beed3ee048f7046fa2e71e959cc78ad97fbf06c
Yumi Nanase <107496047+yuminanase@users.noreply.github.com>
2023-09-15 09:10:33 +0800

    Update botInfo.js

--- commands/botInfo.js
+++ commands/botInfo.js
@@ -4,12 +4,12 @@ module.exports = {
     aliases: ["botinfo", "binfo", "boti"],
     code: `$author[Bot Info;$userAvatar[$botID]]
 $description[**Uptime:** \`$parseMS[$uptime;2;;true]\`
-**ForgeScript Version:** \`$version\`
-**Node.JS Version:** \`$nodeVersion\`
+**ForgeScript:** \`v$version\`
+**Node.JS:** \`$nodeVersion\`
 **RAM:** \`$floor[$ram]MB\`
 **CPU:** \`$math[$cpu/100]%\`
 **Guilds:** \`$guildCount\`
 **Users:** \`$userCount\`]
 $color[FFFFFF]
-$footer[Made with ❤️ by $userDisplayName[$botOwnerID] with BotForge;$userAvatar[$botOwnerID]]`
+$footer[Made with ❤️ by $userDisplayName[$botOwnerID] with ForgeScript;$userAvatar[$botOwnerID]]`
 }
