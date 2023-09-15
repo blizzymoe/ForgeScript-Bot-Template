8a334198e51ccbf0154ad543590139273c97ac03
Yumi Nanase <107496047+yuminanase@users.noreply.github.com>
2023-09-15 12:08:05 +0800

    Update botInfo.js

--- commands/botInfo.js
+++ commands/botInfo.js
@@ -4,12 +4,26 @@ module.exports = {
     aliases: ["botinfo", "binfo", "boti"],
     code: `$author[Bot Info;$userAvatar[$botID]]
 $description[**Uptime:** \`$parseMS[$uptime;2;;true]\`
-**ForgeScript:** \`v$version\`
-**Node.JS:** \`$nodeVersion\`
-**RAM:** \`$floor[$ram]MB\`
-**CPU:** \`$math[$cpu/100]%\`
 **Guilds:** \`$guildCount\`
-**Users:** \`$userCount\`]
+**Users:** \`$userCount\`;1]
+
+$description[**Version:**
+**ForgeScript:** \`v$version\`
+**Node.JS:** \`$nodeVersion\`;2]
+
+$description[**Memory Stats:** **Usage:** \`$floor[$ram]MB\`
+**Free:** \`$cropText[$divide[$djsEval[require("os").freemem()];1073741824];0;2]GB\`
+**Total:** \`$cropText[$divide[$djsEval[require("os").totalmem()];1073741824];0;2]GB\`;3]
+
+$description[**System Stats:**
+**Usage:** \`$math[$cpu/100]%\`
+**Cores:** \`$djsEval[require("os").cpus().length]\`
+**OS:** \`$djsEval[require("os").type()] $djsEval[require("os").release()] $djsEval[require("os").arch()]\`;4]
+
 $color[FFFFFF]
-$footer[Made with ❤️ by $userDisplayName[$botOwnerID] with ForgeScript;$userAvatar[$botOwnerID]]`
+$color[FFFFFF;2]
+$color[FFFFFF;3]
+$color[FFFFFF;4]
+
+$footer[Made with ❤️ by $userDisplayName[$botOwnerID] with ForgeScript;$userAvatar[$botOwnerID];4]`
 }
