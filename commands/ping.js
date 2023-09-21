f1c71319ec231ba0c31a7c8f79eb4e29a2c5a800
Yumi Nanase <107496047+yuminanase@users.noreply.github.com>
2023-09-21 10:34:32 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -7,7 +7,6 @@ $let[1st;$ping]
 $wait[1000]
 $function[$editMessage[$channelID;$get[msg];$title[🏓 | Pong!]
 $description[**Client Latency:** \`$get[1st]ms\`
-**Roundtrip:** \`$math[$executionTime-1000]ms\`
-**Database:** \`$cropText[$randomNumber[0;1;true];0;3]ms\`]
+**Roundtrip:** \`$math[$executionTime-1000]ms\`]
 $color[FFFFFF]]]`
 }
